5b1a39d2271b2b0be919ae62569440ea9daf08d3
mcostales84 <mcostales84@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-19 14:04:01 +0000

    Tagging 1.0
    git-svn-id: https://plugins.svn.wordpress.org/bootstrap-integration/tags/1.0@1094324 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bootstrap-integration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Bootstrap Integration
+ * Description: For those templates which don't came with bootstrap libraries, this plugin integrate the style and the script (version 3.3.2) with your wordpress installation. 
+ * Version: 1.0
+ * Author: Manuel Costales
+ * Author URI: http://www.manuelcostales.com
+ */
+defined( 'ABSPATH' ) or die( 'Plugin file cannot be accessed directly.' );
+/*add javascripts to the header*/
+add_action('wp_head', 'add_bootstrap_mc');
+function add_bootstrap_mc() {
+	wp_enqueue_style( 'bootstrap_style',  plugins_url( 'css/bootstrap.min.css', __FILE__ ) );
+	wp_enqueue_script( 'bootstrap_script', plugins_url( 'js/bootstrap.min.js', __FILE__ ) );
+}
